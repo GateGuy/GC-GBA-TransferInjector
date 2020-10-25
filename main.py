1003cd3e46aa5bb2a0681b20aff3e1605b1eba31
GateGuy <57763469+GateGuy@users.noreply.github.com>
2020-10-25 16:35:49 -0400

    Clarified help

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def initTempFolder():
 	mkdir(tempGCFolder)
 
 def printHelp():
-	print("\nUsage: GCGBA_ti.exe -a <input GBA> -c <input GC> --err <TPL> --load <TPL> --ind <TPL> --done <TPL>")
+	print("\nUsage: GCGBA_ti.exe -a <input GBA> -c <input GC> --err <TPL/PNG> --load <TPL/PNG> --ind <TPL/PNG> --done <TPL/PNG>")
 	print()
 	print("-a <the GBA ROM that will be injected>")
 	print("-c <the Gamecube file that the GBA ROM will be injected into; this is wario_agb.tgc, which can be extracted from the Nintendo Gamecube Preview Disc>")
